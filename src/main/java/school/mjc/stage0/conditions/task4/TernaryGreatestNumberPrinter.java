97492b62a15845d1b1a2a90d446d23e80563e4af
elyad <ella.dikovich@gmail.com>
2022-11-01 16:14:07 +0100

    rttyfffff

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,12 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public static void main(String[] args) {
-        printGreatest(5, 6);
+        printGreatest(15, 1);
     }
     public static void printGreatest(int first, int second) {
-        int result = 0;
-            result = first>=second ? first:second;
-            System.out.println(result);
+
+            System.out.println(first>=second ? first:second);
 
         }
 
